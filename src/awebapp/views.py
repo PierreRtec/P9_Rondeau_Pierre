cedc202e8758b7a6911248f3e4e0d5bda08cedea
PierreRtec <pierre.rondeau9pro@gmail.com>
2022-02-03 11:01:58 +0100

    dernieres modifs views create ticket blocage

--- src/awebapp/views.py
+++ src/awebapp/views.py
@@ -95,20 +95,24 @@ def abos(request):
 def create_ticket(request):
     if request.method == "POST":
         ticket = request.POST.get("ticket")
-        form_ticket = request.POST.get("ticket_form")
-        if form_ticket:
-            try:
-                ticket = Ticket.objects.get(ticket=create_ticket)
-            except:
-                return render(
-                    request,
-                    "reviews/flux.html",
-                    {"erreur": "Ticket non valide et non envoyé"},
-                )
-            Ticket.objects.create(ticket=request.ticket, form_ticket=request.form_ticket)
-        else:
-                form_ticket = UploadTicketForm(initial={"user": request.user})
-                return render(request, "reviews/create-ticket.html", {'form_ticket': form_ticket})
+        form_ticket = request.POST.get("form_ticket")
+        if form_ticket.is_valid():
+            form_ticket.save()
+            return HttpResponseRedirect(reverse("awebapp/posts"))
+    else:
+        form_ticket = UploadTicketForm(
+            initial={
+                "user": request.user,
+                "title": ticket.title,
+                "description": ticket.description,
+                "image": ticket.image,
+            }
+        )
+        return render(
+            request,
+            "reviews/create-ticket.html",
+            {"ticket": ticket, "form_ticket": form_ticket},
+        )
     return render(request, "reviews/create-ticket.html")
 
 
